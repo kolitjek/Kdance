0c11f401edd5c41ea11e085f80d1411155c5323e
klem95 <enxbox@hotmail.com>
2020-12-03 16:49:57 +0100

    updated udp

--- body-tracking-samples/jump_analysis_sample/Networking.py
+++ body-tracking-samples/jump_analysis_sample/Networking.py
@@ -4,25 +4,36 @@ import numpy as np
 
 
 class UDP:
-    def __init__(self):
-        self.UDP_IP = "192.168.48.67"  # Should change to something like 192.168.x.x
-        self.UDP_port = 5005
+    UDP_IP = "192.168.99.1"  # Has to be send on the client side (sender)
+    UDP_port = 5005
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def sender(self, predictions=[], skeleton_data=[]):
+
+class Sender(UDP):
+    def __init__(self):
         print("Sending to UDP target IP: %s" % self.UDP_IP)
         print("Sending to UDP target port: %s" % self.UDP_port)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        composed_list = [predictions, skeleton_data]
-        sock.sendto(bytes(str(composed_list).encode('utf-8')), (self.UDP_IP, self.UDP_port))
 
-    def receiver(self):
+    def broadcast(self, predictions=[], skeleton_data=[]):
+        composed_list = [predictions, skeleton_data]
+        self.sock.sendto(bytes(str(composed_list).encode('utf-8')), (self.UDP_IP, self.UDP_port))
+
+
+class Receiver(UDP):
+    def __init__(self):
+        hostname = socket.gethostname()
+        self.UDP_IP = socket.gethostbyname(hostname)
+        print("Host name: ", hostname)
         print("Listening to UDP target IP: %s" % self.UDP_IP)
         print("Listening to UDP target port: %s" % self.UDP_port)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((self.UDP_IP, self.UDP_port))
 
+        ## getting the IP address using socket.gethostbyname() method
+        self.UDP_IP = socket.gethostbyname(hostname)
+        self.sock.bind((self.UDP_IP, self.UDP_port))
+
+    def listen (self):
         while True:
-            data, addr = sock.recvfrom(1024)
-            print("received message: %s" % data)
+            data, addr = self.sock.recvfrom(1024)
+            # print("received message: %s" % data)
             res = ast.literal_eval(data.decode("utf-8"))
             return res
